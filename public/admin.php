16459da8ad7a98a2d82044b5d214501603f12b3c
Travis Taylor <travis@webdesignofyork.com>
2014-03-25 11:39:45 -0400

    initial commit

---
+++ public/admin.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="css/public.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="header">
+		<h1>Widget Corp</h1>
+	</div>
+	<div id="main">
+		<div id="navigation">
+			&nbsp;
+		</div>
+		<div id="page">
+			<h2>Admin Menu</h2>
+			<p>Welcome to the admin area.</p>
+			<ul>
+				<li><a href="manage_content.php">Manage Website</a></li>
+				<li><a href="manage_admins.php">Manage Admin Users</a></li>
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="footer">Copyright 20xx, Widget Corp</div>
+</body>
+</html>
